fe67181115bfa1d21407ede66491a774ab1cf8f3
giritharan123456 <tgiri7705@gmail.com>
2026-07-08 18:08:58 +0530

    fix: add unhandled rejection handlers to api/index.js for error visibility

--- api/index.js
+++ api/index.js
@@ -1,2 +1,10 @@
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION:', err.message || err);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err.message || err);
+});
+
 const app = require('../server/server');
 module.exports = app;
